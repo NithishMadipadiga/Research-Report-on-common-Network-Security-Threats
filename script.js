499e6ca1db8e6c67583e0425ab7b2b018cce86de
NithishMadipadiga <nithishmadipadiga@gmail.com>
2026-06-15 16:19:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    document.getElementById("result").innerHTML =
+    "Network security threats continue to evolve. Organizations must adopt strong security practices to protect data and systems.";
+}
